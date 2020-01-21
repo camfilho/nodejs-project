120754297727f6ce5db5dac52c8f4173f2fdf5f1
camfilho <camfilho@ufrn.edu.br>
2020-01-21 17:58:25 -0300

    add resets global and form

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,43 @@
 import React from "react";
-
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="app">
+      <aside>
+        <strong>Sign Up</strong>
+        <form action="">
+          <div className="input-block">
+            <label htmlFor="github_username">Github User</label>
+            <input
+              name="github_username"
+              id="github_username"
+              type="text"
+              required
+            />
+          </div>
+          <div id="input-block">
+            <label htmlFor="techs">Stacks</label>
+            <input name="techs" id="techs" type="text" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" type="text" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" type="text" required />
+            </div>
+          </div>
+
+          <button type="submit">Save</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
